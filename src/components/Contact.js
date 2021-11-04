8e1912930302bef68f2591ebc6c2fbce74cd6729
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-11-04 10:34:51 -0400

    wip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Paper, Button, Typography, Grid } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -7,96 +7,119 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import pdf from "../assets/resume.pdf";
 
 const Contact = () => {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
   const onResumeClick = () => {
     window.open(pdf);
   };
-
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          el: myRef.current,
+          mouseControls: false,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x383d3b,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
   return (
-    <div>
-      <Typography marginTop="2%" align="center" variant="h3">
-        Contact Me
-      </Typography>
-      <Grid container justifyContent={"center"} sx={{ marginTop: "2%" }}>
-        <Grid item lg={4} md={6} sm={8} xs={10}>
-          <Paper
-            sx={{
-              display: "flex",
-              flexFlow: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              textAlign: "center",
-              padding: "7.5% 0",
-              backgroundColor: "primary.main",
-              margin: "5% 0",
-              height: "30vh",
-            }}
-          >
-            <Button
-              target="_blank"
-              href="https://www.linkedin.com/in/shanegray394/"
+    <div ref={myRef}>
+      <div>
+        <Typography marginTop="2%" align="center" variant="h3">
+          Contact Me
+        </Typography>
+        <Grid container justifyContent={"center"} sx={{ marginTop: "2%" }}>
+          <Grid item lg={4} md={6} sm={8} xs={10}>
+            <Paper
               sx={{
-                margin: "1% 90%",
-                maxWidth: "500px",
-                maxHeight: "50px",
-                minWidth: "120px",
-                minHeight: "30px",
+                display: "flex",
+                flexFlow: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                padding: "7.5% 0",
+                backgroundColor: "primary.main",
+                margin: "5% 0",
+                height: "30vh",
               }}
-              variant="contained"
-              startIcon={<LinkedInIcon />}
-              color="secondary"
             >
-              LinkedIN
-            </Button>
-            <Button
-              target="_blank"
-              href="https://twitter.com/Shane_the_Gray"
-              sx={{
-                margin: "1% 90%",
-                maxWidth: "500px",
-                maxHeight: "50px",
-                minWidth: "120px",
-                minHeight: "30px",
-              }}
-              variant="contained"
-              startIcon={<TwitterIcon />}
-              color="secondary"
-            >
-              Twitter
-            </Button>
-            <Button
-              target="_blank"
-              href="https://github.com/Shane-Gray394"
-              sx={{
-                margin: "1% 90%",
-                maxWidth: "500px",
-                maxHeight: "50px",
-                minWidth: "120px",
-                minHeight: "30px",
-              }}
-              variant="contained"
-              startIcon={<GitHubIcon />}
-              color="secondary"
-            >
-              GitHub
-            </Button>
-            <Button
-              onClick={onResumeClick}
-              sx={{
-                margin: "1% 90%",
-                maxWidth: "500px",
-                maxHeight: "50px",
-                minWidth: "120px",
-                minHeight: "30px",
-              }}
-              variant="contained"
-              startIcon={<DescriptionIcon />}
-              color="secondary"
-            >
-              Resume
-            </Button>
-          </Paper>
+              <Button
+                target="_blank"
+                href="https://www.linkedin.com/in/shanegray394/"
+                sx={{
+                  margin: "1% 90%",
+                  maxWidth: "500px",
+                  maxHeight: "50px",
+                  minWidth: "120px",
+                  minHeight: "30px",
+                }}
+                variant="contained"
+                startIcon={<LinkedInIcon />}
+                color="secondary"
+              >
+                LinkedIN
+              </Button>
+              <Button
+                target="_blank"
+                href="https://twitter.com/Shane_the_Gray"
+                sx={{
+                  margin: "1% 90%",
+                  maxWidth: "500px",
+                  maxHeight: "50px",
+                  minWidth: "120px",
+                  minHeight: "30px",
+                }}
+                variant="contained"
+                startIcon={<TwitterIcon />}
+                color="secondary"
+              >
+                Twitter
+              </Button>
+              <Button
+                target="_blank"
+                href="https://github.com/Shane-Gray394"
+                sx={{
+                  margin: "1% 90%",
+                  maxWidth: "500px",
+                  maxHeight: "50px",
+                  minWidth: "120px",
+                  minHeight: "30px",
+                }}
+                variant="contained"
+                startIcon={<GitHubIcon />}
+                color="secondary"
+              >
+                GitHub
+              </Button>
+              <Button
+                onClick={onResumeClick}
+                sx={{
+                  margin: "1% 90%",
+                  maxWidth: "500px",
+                  maxHeight: "50px",
+                  minWidth: "120px",
+                  minHeight: "30px",
+                }}
+                variant="contained"
+                startIcon={<DescriptionIcon />}
+                color="secondary"
+              >
+                Resume
+              </Button>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
