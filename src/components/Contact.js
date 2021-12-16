d25a0ace6526a474a463b5dc2bbfd1e00ca2e464
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-12-16 18:04:07 -0500

    wip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Paper, Button, Typography, Grid } from "@mui/material";
+import { Paper, Button, Typography, Grid, TextField } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 import DescriptionIcon from "@mui/icons-material/Description";
 import WAVES from "vanta/dist/vanta.waves.min";
 
 const Contact = () => {
   const [vantaEffect, setVantaEffect] = useState(0);
   const myRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.open(
+      String("mailto:shane.dalton.gray^gmail.com").replace("^", "@"),
+      "_self"
+    );
+  };
 
   useEffect(() => {
     if (!vantaEffect) {
@@ -36,6 +45,7 @@ const Contact = () => {
         container
         justifyContent={"center"}
         alignItems={"center"}
+        columnSpacing={4}
         sx={{ height: "80vh" }}
       >
         <Grid item lg={4} md={6} sm={8} xs={10}>
@@ -115,6 +125,27 @@ const Contact = () => {
               color="secondary"
             >
               <Typography color={"text.main"}>GitHub</Typography>
+            </Button>
+            <Button
+              onClick={handleSubmit}
+              sx={{
+                margin: "1% 90%",
+                maxWidth: "500px",
+                maxHeight: "50px",
+                minWidth: "140px",
+                minHeight: "40px",
+              }}
+              variant="contained"
+              startIcon={
+                <EmailIcon
+                  sx={{
+                    color: "text.main",
+                  }}
+                />
+              }
+              color="secondary"
+            >
+              <Typography color={"text.main"}>Email Me</Typography>
             </Button>
             <Button
               href="https://docs.google.com/document/d/1eQWS-g4Ze0B4YtK5YmNFScb46QunMcKAzSSEG7xZlKs/edit?usp=sharing"
