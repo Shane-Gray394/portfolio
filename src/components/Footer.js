35af2f215fb6658004572bfad6d36010774cc06c
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-12-30 14:33:32 -0500

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,35 @@
-import React from "react";
-import { Typography, Box } from "@mui/material";
+import React from 'react';
+import { Typography, Box, Link } from '@mui/material';
 
 const Footer = () => {
   return (
     <Box
       sx={{
-        backgroundColor: "primary.main",
-        height: "10vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-end",
-        paddingBottom: "0.6%",
+        backgroundColor: 'primary.main',
+        height: '10vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        paddingBottom: '0.6%',
       }}
     >
-      <Typography variant={"h8"} color={"text.main"} fontSize={13}>
+      <Typography variant={'h8'} color={'text.main'} fontSize={13} pr={1}>
         Created by Shane Gray &copy; 2021
+      </Typography>
+      <Typography variant={'h8'} color={'text.main'} fontSize={13}>
+        &#x2759;
+      </Typography>
+
+      <Typography variant={'h8'} color={'text.main'} fontSize={13} pl={1}>
+        Logo Created by{' '}
+        <Link
+          sx={{ textDecoration: 'none', color: 'text.main' }}
+          target="_blank"
+          href="https://www.linkedin.com/in/william-paris-jr-692415154/"
+        >
+          William Paris
+        </Link>{' '}
+        &copy; 2021
       </Typography>
     </Box>
   );
